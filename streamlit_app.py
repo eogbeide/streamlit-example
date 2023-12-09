76348fdff7c94a4370ba23fffdc59eab59850716
eogbeide <50998708+eogbeide@users.noreply.github.com>
2023-12-09 14:38:00 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,154 @@
 import altair as alt
 import numpy as np
 import pandas as pd
+import plotly.express as px
+from prophet import Prophet
+from plotly import graph_objs as go
+import glob
+from pandas_datareader import data as pdr
+from datetime import date
+import yfinance as yf
+import os
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+yf.pdr_override()
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Tickers list
+ticker_list = ['MTCH', 'EA', 'PYPL', 'INTC', 'PFE', 'MRNA', 'VWAPY', 'CRL', 'CRM', 'AFRM', 'MU', 'AMAT', 'DELL', 'HPQ', 'BABA', 'VTWG', 'SPGI', 'STX', 'LABU', 'TSM', 'AMZN', 'BOX', 'AAPL', 'NFLX', 'AMD', 'GME', 'GOOG', 'GUSH', 'LU', 'META', 'MSFT', 'NVDA', 'PLTR', 'SITM', 'SPCE', 'SPY', 'TSLA', 'URI', 'WDC']
+today = date.today()
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# We can get data by our choice by giving days bracket
+start_date = "2021-12-01"
+end_date = today.strftime("%Y-%m-%d")  # Use today's date as the end date
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+files = []
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+def getData(ticker):
+    print(ticker)
+    data = pdr.get_data_yahoo(ticker, start=start_date, end=today)
+    dataname = ticker + '_' + str(today)
+    files.append(dataname)
+    SaveData(data, dataname)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+# Create a data folder in your current dir.
+def SaveData(df, filename):
+    save_path = os.path.expanduser('~/Documents/data/')
+    df.to_csv(os.path.join(save_path, filename + '.csv'))
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+# This loop will iterate over ticker list, will pass one ticker to get data, and save that data as a file.
+for tik in ticker_list:
+    getData(tik)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# Pull data, train model, and predict
+def select_files(files):
+    num_files = len(files)
+
+    # Print the list of files
+    for i, file in enumerate(files):
+        print(f"{i + 1}. {file}")
+
+    selected_files = []
+    # Prompt the user to choose two files
+    for _ in range(2):
+        while True:
+            try:
+                choice = st.sidebar.selectbox("Select a file", range(1, num_files + 1), format_func=lambda x: files[x - 1].split('/')[-1].split('_')[0], key=f"selectbox_{_}")
+                selected_file = files[choice - 1]
+                selected_files.append(selected_file)
+                break
+            except IndexError:
+                st.sidebar.warning("Invalid choice. Please try again.")
+
+    return selected_files
+
+# the path to your csv file directory
+mycsvdir = 'C:/Users/eogbeide/Documents/data'
+
+# get all the csv files in that directory (assuming they have the extension .csv)
+csvfiles = glob.glob(os.path.join(mycsvdir, '*.csv'))
+
+# Prompt the user to select two files
+selected_files = select_files(csvfiles)
+
+# Read the selected files using pandas
+dfs = []
+for selected_file in selected_files:
+    df = pd.read_csv(selected_file)
+    df = df[['Date', 'Close']]
+    df.columns = ['ds', 'y']
+    df['ds'] = pd.to_datetime(df['ds'])
+    df.reset_index(inplace=True, drop=True)
+    dfs.append(df)
+
+# Plot the selected files
+titles = []
+tickers = []
+for selected_file in selected_files:
+    ticker = selected_file.split('/')[-1].split('_')[0]
+    tickers.append(ticker)
+    selected_file = selected_file.replace(mycsvdir + '/', '')  # Remove the directory path
+    selected_file = selected_file.replace('.csv', '')  # Remove the ".csv" extension
+    selected_file = selected_file.replace('data"\"', '')  # Remove the ".data" extension
+    ticker = ticker.replace('data"\"', '')  # Remove the ".data" extension
+    #titles.append(f'Original Vs Predicted ({ticker})')
+    titles.append(f'Chart of Original Vs Predicted for ({ticker})')
+
+def interactive_plot_forecasting(df, forecast, title):
+    fig = px.line(df, x='ds', y=['y', 'predicted'], title=title)
+
+    # Get maximum and minimum points
+    max_points = df[df['y'] == df['y'].max()]
+    min_points = df[df['y'] == df['y'].min()]
+
+    # Add maximum points to the plot
+    fig.add_trace(go.Scatter(x=max_points['ds'], y=max_points['y'], mode='markers', name='Maximum'))
+
+    # Add minimum points to the plot
+    fig.add_trace(go.Scatter(x=min_points['ds'], y=min_points['y'], mode='markers', name='Minimum'))
+
+    # Add yhat_lower and yhat_upper
+    fig.add_trace(go.Scatter(x=df['ds'], y=forecast['yhat_lower'], mode='lines', name='yhat_lower'))
+    fig.add_trace(go.Scatter(x=df['ds'], y=forecast['yhat_upper'], mode='lines', name='yhat_upper'))
+
+    st.plotly_chart(fig)
+
+# Append today's date to the titles
+today = date.today().strftime("%Y-%m-%d")
+
+# Iterate over the selected files and their corresponding titles
+for df, title, ticker in zip(dfs, titles, tickers):
+    # Split the data into testing and training datasets
+    train = df[df['ds'] <= '10/31/2023']
+    test = df[df['ds'] >= '11/01/2023']
+
+    st.title("Major US Stocks Forecast Wizard")
+    st.write("")
+    st.subheader("The Smart Stock Trend Wiz: $$$")
+    st.write({ticker})
+    st.write("How to read chart: Below yhat_lower --> buy signal, above yhat_upper --> sell signal")
+    #st.write(f"Number of months in train data for {ticker}: {len(train)}")
+    #st.write(f"Number of months in test data for {ticker}: {len(test)}")
+    st.write(f"Number of days in train data: {len(train)}")
+    st.write(f"Number of days in test data: {len(test)}")
+
+    # Initialize Model
+    m = Prophet()
+
+    # Create and fit the prophet model to the training data
+    m.fit(train)
+
+    # Make predictions
+    future = m.make_future_dataframe(periods=93)
+    forecast = m.predict(future)
+    #st.write("Forecast for", ticker)
+    #   st.write(forecast[['ds', 'yhat_lower', 'yhat', 'yhat_upper']].tail(30))
+
+    # Add predicted values to the original dataframe
+    df['predicted'] = forecast['trend']
+
+    # Plot the forecast and the original values for comparison
+    interactive_plot_forecasting(df, forecast, f'{title} ({today})')
+
+# Delete existing files
+for file in csvfiles:
+    os.remove(file)
